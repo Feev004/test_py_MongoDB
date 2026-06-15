5a0150a96e051f81d2d809a9c66a83af44863cf7
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-06-16 02:34:03 +0700

    up

--- delete.py
+++ delete.py
@@ -2,13 +2,18 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017/')
 db = client['MyDB']
 td = db['myCollection_customers']
-delete_result = td.delete_many({"age": 35})
-print(f"Deleted {delete_result.deleted_count} documents with age 35")
+# delete_result = td.delete_many({"age": 35})
 # td.delete_one({}) 
-# td.delete_many({"id": 1})
-# td.delete_many({"id": 2})
-# td.delete_many({"id": 3})
-# td.delete_many({"id": 4})
-# td.delete_many({"id": 5})
+# delete_result =td.delete_many({"id": 1})
+# delete_result =td.delete_many({"id": 2})
+# delete_result =td.delete_many({"id": 3})
+# delete_result =td.delete_many({"id": 4})
+# delete_result =td.delete_many({"id": 5})
+num = 0
+for customer in td.find():
+    num += 1
+print(num)
+for i in range(1, num+1):
+    delete_result = td.delete_many({"id": i})
 
-print("Data deleted successfully")
+print(f"Deleted {delete_result.deleted_count} documents")
